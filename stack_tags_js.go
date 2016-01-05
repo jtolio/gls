bba90643f6f2c6c68d115f47c926342589720d11
Jonathan Hall <flimzy@flimzy.com>
2016-01-05 21:56:22 +0100

    Re-factor stack_tags*.go to remove duplicate code.
     - stack_tags.go contains common code, useable in both Go and GopherJS runtime environments
     - stack_tags_js.go contains the GopherJS/Node.js specific runtime stack handling
     - stack_tags_main.go contains the standard Go runtime specific stack handling

--- stack_tags_js.go
+++ stack_tags_js.go
@@ -12,38 +12,6 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-const (
-	bitWidth = 4
-)
-
-func addStackTag(tag uint, context_call func()) {
-	if context_call == nil {
-		return
-	}
-	markS(tag, context_call)
-}
-
-func markS(tag uint, cb func()) { _m(tag, cb) }
-func mark0(tag uint, cb func()) { _m(tag, cb) }
-func mark1(tag uint, cb func()) { _m(tag, cb) }
-func mark2(tag uint, cb func()) { _m(tag, cb) }
-func mark3(tag uint, cb func()) { _m(tag, cb) }
-func mark4(tag uint, cb func()) { _m(tag, cb) }
-func mark5(tag uint, cb func()) { _m(tag, cb) }
-func mark6(tag uint, cb func()) { _m(tag, cb) }
-func mark7(tag uint, cb func()) { _m(tag, cb) }
-func mark8(tag uint, cb func()) { _m(tag, cb) }
-func mark9(tag uint, cb func()) { _m(tag, cb) }
-func markA(tag uint, cb func()) { _m(tag, cb) }
-func markB(tag uint, cb func()) { _m(tag, cb) }
-func markC(tag uint, cb func()) { _m(tag, cb) }
-func markD(tag uint, cb func()) { _m(tag, cb) }
-func markE(tag uint, cb func()) { _m(tag, cb) }
-func markF(tag uint, cb func()) { _m(tag, cb) }
-
-var pc_lookup = make(map[uintptr]int8, 17)
-var mark_lookup [16]func(uint, func())
-
 var stackRE = regexp.MustCompile("\\s+at (\\S*) \\([^:]+:(\\d+):(\\d+)")
 
 func findPtr() uintptr {
@@ -94,14 +62,6 @@ func init() {
 	setEntries(markD, 0xd)
 	setEntries(markE, 0xe)
 	setEntries(markF, 0xf)
-}
-
-func _m(tag_remainder uint, cb func()) {
-	if tag_remainder == 0 {
-		cb()
-	} else {
-		mark_lookup[tag_remainder&0xf](tag_remainder>>bitWidth, cb)
-	}
 }
 
 func currentStack(skip int) (stack []uintptr) {
